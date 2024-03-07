115de27c295513704e629f47d120fc9089e7e751
dericko681 <abonghoderick@gmail.com>
2024-03-07 06:58:10 +0100

    the activities of the ask phase of my data analysis

---
+++ ask_phase.R
@@ -0,0 +1,15 @@
+This is the first phase of my data analysis life cycle. For this project, In this phase I caried out investigations to understand my stakeholders and the problem they will like me to solve
+
+scenario:
+You are a junior data analyst working on the marketing analyst team at Cyclistic, a bike-share
+company in Chicago. The director of marketing believes the company’s future success
+depends on maximizing the number of annual memberships. Therefore, your team wants to
+understand how casual riders and annual members use Cyclistic bikes differently. From these
+insights, your team will design a new marketing strategy to convert casual riders into annual
+members. But first, Cyclistic executives must approve your recommendations, so they must be
+backed up with compelling data insights and professional data visualizations.
+
+problem definition:
+How do annual members and casual
+riders use Cyclistic bikes differently?
+
